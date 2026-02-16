2de0c8cff3cdd1fc1d25204e00a29246f55fadd1
Daniel Garcia <github@mail.daniel.es>
2026-02-16 11:39:35 +0000

    Initial landing

--- src/config.ts
+++ src/config.ts
@@ -14,7 +14,8 @@ type SiteConfig = {
 type Link = {
     text: string;
     href: string;
-};
+    target?: string;
+    };
 
 type Action = {
     href: string;
@@ -53,13 +54,13 @@ type NavigationConfig = {
 
 export const SITE = (): SiteConfig => {
     return {
-        title: "MyWebsite - This is my website",
+        title: "CLEMENT Puerta de Hierro - Chalets de Gran Lujo en Madrid",
         description:
-            "This is my website description",
-        author: "My Name",
+            "Descubre la magia de los chalets de Gran Lujo en Madrid. Disfruta de una experiencia única en un entorno tranquilo y natural, perfecto para relajarte y disfrutar de la naturaleza.",
+        author: "Clement",
         siteUrl: "https://mywebsite.com/",
         ogImage: "/src/assets/images/og-image.jpg", // Needs to be an absolute path /src/...
-        locale: "en_US",
+        locale: "es_ES",
         twitter: { site: "@mywebsite" },
     };
 };
@@ -67,20 +68,22 @@ export const SITE = (): SiteConfig => {
 export const NAVIGATION = (): NavigationConfig => ({
     header: {
         links: [
-            { text: "Home", href: "/" },
-            { text: "About", href: "/about/" },
-            { text: "Services", href: "/services/" },
-            { text: "Contact", href: "/contact/" },
+            { text: "Introducción", href: "/#introduccion" },
+            { text: "La Promoción", href: "/#vista-aerea" },
+            { text: "Ubicación", href: "/#ubicacion" },
+            { text: "Calidades", href: "/#calidades" },
+            { text: "Viviendas", href: "/#viviendas" },
+            { text: "Contacto", href: "/#contacto" },
         ],
         actions: [
             {
-                href: `/#hero`,
-                text: "Start syncing",
-                icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-rocket"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 13a8 8 0 0 1 7 7a6 6 0 0 0 3 -5a9 9 0 0 0 6 -8a3 3 0 0 0 -3 -3a9 9 0 0 0 -8 6a6 6 0 0 0 -5 3" /><path d="M7 14a6 6 0 0 0 -3 6a6 6 0 0 0 6 -3" /><path d="M14 9a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /></svg>`
+                href: `/AF_Dossier Puerta de Hierro.pdf`,
+                text: "Descargar dossier",
+                icon: `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="7 10 12 15 17 10"/><line x1="12" y1="15" x2="12" y2="3"/></svg>`
             },
             {
-                href: `/contact/`,
-                text: "Contact",
+                href: `/#contacto`,
+                text: "Contacto",
             },
         ],
     },
@@ -88,49 +91,37 @@ export const NAVIGATION = (): NavigationConfig => ({
     footer: {
         links: [
             {
-                title: "Support",
+                title: "CLEMENT Puerta de Hierro",
                 links: [
-                    { text: "Contact", href: `/contact/` },
-                    { text: "Blog", href: `/blog/` },
-                    { text: "Guides", href: `/blog/guides/` },
+                    { text: "Contacto", href: "#contacto" },
                 ],
             },
             {
-                title: "MyWebsite",
+                title: "Clement",
                 links: [
-                    { text: "How it works", href: `/#features` },
-                    { text: "About us", href: `/#about` },
-                ],
-            },
-            {
-                title: "GearShift Universe",
-                links: [
-                    { text: "GearShift", href: "https://gearshift.es/" },
-                    { text: "postify AI", href: "https://postifyai.com/" },
-                    { text: "Estavia", href: "https://estavia.ai/" },
-                    { text: "AutoIPC", href: "https://autoipc.es/" },
-                    { text: "SynCal", href: "https://syncal.app/" },
+                    { text: "Quiénes somos", href: "#nosotros" },
+                    { text: "Otras promociones", href: "https://clement.es/", target: "_blank" },
                 ],
             },
         ],
         secondaryLinks: [
-            { text: "Terms and conditions", href: `/terms/` },
-            { text: "Privacy policy", href: `/privacy/` },
+            { text: "Términos y condiciones", href: `/terms/` },
+            { text: "Política de privacidad", href: `/privacy/` },
         ],
         socialLinks: [
-            {
-                ariaLabel: "X",
-                icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-brand-x"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4l11.733 16h4.267l-11.733 -16l-4.267 0" /><path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772" /></svg>`,
-                href: "https://x.com/mywebsite",
-            },
+            // {
+            //     ariaLabel: "X",
+            //     icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-brand-x"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4l11.733 16h4.267l-11.733 -16l-4.267 0" /><path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772" /></svg>`,
+            //     href: "https://x.com/mywebsite",
+            // },
             // { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://instagram.com/mywebsite' },
             // { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://facebook.com/mywebsite' },
-            { ariaLabel: "RSS", icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-rss"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 19a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M4 4a16 16 0 0 1 16 16" /><path d="M4 11a9 9 0 0 1 9 9" /></svg>`, href: `/rss.xml` },
+            // { ariaLabel: "RSS", icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-rss"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 19a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M4 4a16 16 0 0 1 16 16" /><path d="M4 11a9 9 0 0 1 9 9" /></svg>`, href: `/rss.xml` },
         ],
         contactDetails: {
-            address: "Calle Santa Leonor, 64, Edifio D, Madrid",
-            addressLink: "https://maps.app.goo.gl/tb4McHigby4r5BeP7",
-            email: "hello@mywebsite.com",
+            address: "Calle Santibáñez de Béjar, 7, 28042, Madrid",
+            addressLink: "https://maps.app.goo.gl/wXeAWh2rh2ssrLxg9",
+            email: "info@clement.es",
         },
         footNote: `
           <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
